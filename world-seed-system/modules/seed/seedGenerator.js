772adf5be5ee3358387d0b4e9b43cb46eec202ee
Cero-Kurn <147090016+Cero-Kurn@users.noreply.github.com>
2026-04-09 09:00:20 -0600

    Update seedGenerator.js

--- world-seed-system/modules/seed/seedGenerator.js
+++ world-seed-system/modules/seed/seedGenerator.js
@@ -1,4 +1,11 @@
-import { CC_TABLE, LM_TABLE, WE_TABLE, TR_TABLE, HY_TABLE, SF_TABLE } from "./lookupTables.js";
+import {
+  CC_TABLE, LM_TABLE, WE_TABLE, TR_TABLE, HY_TABLE, SF_TABLE
+} from "./lookupTables.js";
+
+import {
+  CC_SECONDARY, LM_SECONDARY, WE_SECONDARY,
+  TR_SECONDARY, HY_SECONDARY, SF_SECONDARY
+} from "./secondaryTables.js";
 
 function randomKey(table) {
   const keys = Object.keys(table);
