0839afed8a4c40a047372307aca34bb7b8a658e8
Cero-Kurn <147090016+Cero-Kurn@users.noreply.github.com>
2026-04-09 07:41:45 -0600

    Create seedGenerator.js

---
+++ world-seed-system/modules/seed/seedGenerator.js
@@ -0,0 +1,16 @@
+import { CC_TABLE, LM_TABLE, WE_TABLE, TR_TABLE, HY_TABLE, SF_TABLE } from "./lookupTables.js";
+
+function randomKey(table) {
+  const keys = Object.keys(table);
+  return keys[Math.floor(Math.random() * keys.length)];
+}
+
+export function generateRandomSeed() {
+  const cc = randomKey(CC_TABLE) + randomKey(CC_TABLE);
+  const lm = randomKey(LM_TABLE) + randomKey(LM_TABLE);
+  const we = randomKey(WE_TABLE) + randomKey(WE_TABLE);
+  const tr = randomKey(TR_TABLE) + randomKey(TR_TABLE);
+  const hy = randomKey(HY_TABLE) + randomKey(HY_TABLE);
+  const sf = randomKey(SF_TABLE) + randomKey(SF_TABLE);
+  return `${cc}-${lm}-${we}-${tr}-${hy}-${sf}`;
+}
